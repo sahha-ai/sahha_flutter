e17cd674bc2f1b47d0a79be674c8c88c2c16cfb9
Min <hcha219@gmail.com>
2022-08-25 15:53:22 +1200

    Add some Sahha theming

--- example/lib/Views/HomeView.dart
+++ example/lib/Views/HomeView.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:io' show Platform;
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -8,114 +7,130 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Sahha Demo'),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 70,
+                width: 70,
+                child: Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: Image.asset(
+                    "assets/logo.png",
+                    fit: BoxFit.scaleDown,
+                  ),
+                ),
+              ),
+              const Text('Sahha Demo'),
+            ],
+          ),
         ),
         body: Padding(
           padding: const EdgeInsets.all(40),
           child: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  ElevatedButton.icon(
-                    icon: const Icon(
-                      Icons.lock,
-                      size: 32,
-                    ),
-                    label: const Text('Authentication'),
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(50),
-                      padding: const EdgeInsets.all(20),
-                      textStyle: const TextStyle(fontSize: 20),
-                    ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/authentication');
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton.icon(
-                    icon: const Icon(
-                      Icons.person,
-                      size: 32,
-                    ),
-                    label: const Text('Profile'),
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(50),
-                      padding: const EdgeInsets.all(20),
-                      textStyle: const TextStyle(fontSize: 20),
-                    ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/profile');
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton.icon(
-                    icon: const Icon(
-                      Icons.dark_mode,
-                      size: 32,
-                    ),
-                    label: const Text('Sleep'),
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(50),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 40, vertical: 20),
-                      textStyle: const TextStyle(fontSize: 20),
-                    ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/sleep');
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton.icon(
-                    icon: const Icon(
-                      Icons.directions_walk,
-                      size: 32,
-                    ),
-                    label: const Text('Pedometer'),
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(50),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 40, vertical: 20),
-                      textStyle: const TextStyle(fontSize: 20),
-                    ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/pedometer');
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton.icon(
-                    icon: const Icon(
-                      Icons.query_stats,
-                      size: 32,
-                    ),
-                    label: const Text('Analyzation'),
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(50),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 40, vertical: 20),
-                      textStyle: const TextStyle(fontSize: 20),
-                    ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/analyzation');
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton.icon(
-                    icon: const Icon(
-                      Icons.bar_chart,
-                      size: 32,
-                    ),
-                    label: const Text('View Sensor Data'),
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(50),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 40, vertical: 20),
-                      textStyle: const TextStyle(fontSize: 20),
-                    ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/sensor_data');
-                    },
-                  ),
-                ]),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+                    Widget>[
+              ElevatedButton.icon(
+                icon: const Icon(
+                  Icons.lock,
+                  size: 32,
+                ),
+                label: const Text('Authentication'),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                  padding: const EdgeInsets.all(20),
+                  textStyle: const TextStyle(fontSize: 20),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/authentication');
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton.icon(
+                icon: const Icon(
+                  Icons.person,
+                  size: 32,
+                ),
+                label: const Text('Profile'),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                  padding: const EdgeInsets.all(20),
+                  textStyle: const TextStyle(fontSize: 20),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/profile');
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton.icon(
+                icon: const Icon(
+                  Icons.dark_mode,
+                  size: 32,
+                ),
+                label: const Text('Sleep'),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                  textStyle: const TextStyle(fontSize: 20),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/sleep');
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton.icon(
+                icon: const Icon(
+                  Icons.directions_walk,
+                  size: 32,
+                ),
+                label: const Text('Pedometer'),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                  textStyle: const TextStyle(fontSize: 20),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/pedometer');
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton.icon(
+                icon: const Icon(
+                  Icons.query_stats,
+                  size: 32,
+                ),
+                label: const Text('Analyzation'),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                  textStyle: const TextStyle(fontSize: 20),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/analyzation');
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton.icon(
+                icon: const Icon(
+                  Icons.bar_chart,
+                  size: 32,
+                ),
+                label: const Text('View Sensor Data'),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                  textStyle: const TextStyle(fontSize: 20),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/sensor_data');
+                },
+              ),
+            ]),
           ),
         ));
   }
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:sahha_flutter/sahha_flutter.dart';
-import 'package:sahha_flutter_example/Views/HomeView.dart';
+import 'package:sahha_flutter_example/Views/AnalyzationView.dart';
 import 'package:sahha_flutter_example/Views/AuthenticationView.dart';
+import 'package:sahha_flutter_example/Views/HomeView.dart';
+import 'package:sahha_flutter_example/Views/PedometerView.dart';
 import 'package:sahha_flutter_example/Views/ProfileView.dart';
 import 'package:sahha_flutter_example/Views/SensorDataView.dart';
 import 'package:sahha_flutter_example/Views/SleepView.dart';
-import 'package:sahha_flutter_example/Views/PedometerView.dart';
-import 'package:sahha_flutter_example/Views/AnalyzationView.dart';
 
 void main() {
   runApp(const App());
@@ -42,17 +42,34 @@ class AppState extends State<App> {
 
   @override
   Widget build(BuildContext context) {
+    const sahhaDark = MaterialColor(0xff333242, <int, Color>{
+      50: Color(0xff333242),
+      100: Color(0xff333242),
+      200: Color(0xff333242),
+      300: Color(0xff333242),
+      400: Color(0xff333242),
+      500: Color(0xff333242),
+      600: Color(0xff333242),
+      700: Color(0xff333242),
+      800: Color(0xff333242),
+      900: Color(0xff333242),
+    });
+
     return MaterialApp(
-      initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) => const HomeView(),
-        '/authentication': (BuildContext context) => const AuthenticationView(),
-        '/profile': (BuildContext context) => const ProfileView(),
-        '/sleep': (BuildContext context) => const SleepView(),
-        '/pedometer': (BuildContext context) => const PedometerView(),
-        '/analyzation': (BuildContext context) => const AnalyzationView(),
-        '/sensor_data': (BuildContext context) => const SensorDataView()
-      },
-    );
+        initialRoute: '/',
+        routes: <String, WidgetBuilder>{
+          '/': (BuildContext context) => const HomeView(),
+          '/authentication': (BuildContext context) =>
+              const AuthenticationView(),
+          '/profile': (BuildContext context) => const ProfileView(),
+          '/sleep': (BuildContext context) => const SleepView(),
+          '/pedometer': (BuildContext context) => const PedometerView(),
+          '/analyzation': (BuildContext context) => const AnalyzationView(),
+          '/sensor_data': (BuildContext context) => const SensorDataView()
+        },
+        theme: ThemeData(
+          fontFamily: 'Rubik',
+          primarySwatch: sahhaDark,
+        ));
   }
 }
