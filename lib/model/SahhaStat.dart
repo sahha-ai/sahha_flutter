6a86541af0f8c23bbb0cdb38b763defdc0a0d866
smileypop <matt@sahha.ai>
2024-12-17 12:00:55 +0900

    change stat type to string

--- lib/model/SahhaStat.dart
+++ lib/model/SahhaStat.dart
@@ -2,7 +2,7 @@ import 'package:sahha_flutter/sahha_flutter.dart';
 
 class SahhaStat {
   final String id = '';
-  final SahhaSensor sensor = SahhaSensor.steps;
+  final String type = '';
   final double value = 0.0;
   final String unit = '';
   final DateTime startDate = DateTime.now();
