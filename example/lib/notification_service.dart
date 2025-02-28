b43f7287abfcff42607507a903f8a52d88a38342
smileypop <matt@sahha.ai>
2025-02-28 14:03:44 +0900

    add notifications

---
+++ example/lib/notification_service.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
+import 'package:flutter_push_notifications/utils/download_util.dart';
+import 'package:rxdart/subjects.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
+class NotificationService {
+  NotificationService();
+
+  final _localNotifications = FlutterLocalNotificationsPlugin();
+  final BehaviorSubject<String> behaviorSubject = BehaviorSubject();
+
+  Future<void> initializePlatformNotifications() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('ic_stat_justwater');
+
+    final IOSInitializationSettings initializationSettingsIOS =
+        IOSInitializationSettings(
+            requestSoundPermission: true,
+            requestBadgePermission: true,
+            requestAlertPermission: true,
+            onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsIOS,
+    );
+
+    await _localNotifications.initialize(initializationSettings,
+        onSelectNotification: selectNotification);
+  }
+
+  void onDidReceiveLocalNotification(
+      int id, String? title, String? body, String? payload) {
+    debugPrint('id $id');
+  }
+
+  void selectNotification(String? payload) {
+    if (payload != null && payload.isNotEmpty) {
+      behaviorSubject.add(payload);
+    }
+  }
+}
